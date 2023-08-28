8a5c4713239c8a91c651c8d57a2722617a49ed26
Dhanvinsh <72690343+Dhanvinsh99@users.noreply.github.com>
2023-08-28 08:41:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, jsonify
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_time', methods=['GET'])
+def get_time():
+    current_time = time.ctime()
+    epoch_time_ns = int(time.time_ns())
+    time_bc_ns = 62125920000000000 + time.time_ns()
+    
+    return jsonify({
+        'current_time': current_time,
+        'epoch_time_ns': epoch_time_ns,
+        'time_bc_ns': time_bc_ns
+    })
+
+if __name__ == '__main__':
+    app.run(port=0, debug=True)
